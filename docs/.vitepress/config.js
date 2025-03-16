3398f9967ee2626ee460dbda0c5164ce3f39e122
Zubulun <zebing.gdut@gmail.com>
2025-03-16 23:21:32 +0800

    ✨feat(package/工程化): 修改配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ import themeConfig from "./config/themeConfig"
 import markdown from "./config/markdown"
 
 export default {
-  title: "Zublun",
+  title: "Aaron",
   lang: 'zh-CN',
   locale: 'zh_CN',
   description: '个人知识库，记录 & 分享个人碎片化、结构化、体系化的知识内容。',
--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -2,8 +2,8 @@ import type { HeadConfig } from 'vitepress'
 
 const head: HeadConfig[] = [
   ['link', { rel: 'icon', href: '/assets/favicon.ico' }],
-  ['meta', { name: 'author', content: 'Zubulun' }],
-  ['meta', { name: 'keywords', content: 'Zubulun知识库, 知识库, 博客' }],
+  ['meta', { name: 'author', content: 'AaronWong' }],
+  ['meta', { name: 'keywords', content: 'AaronWong知识库, 知识库, 博客' }],
 ]
 
 export default head
